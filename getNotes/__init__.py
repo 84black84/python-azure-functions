b6148657018a95715455cacbae7c202173298953
Christos Mavrogiannis <christos.mavrogiannis@ottobock.de>
2021-05-22 21:58:14 +0200

    getNotes - simple change for testing purposes

--- getNotes/__init__.py
+++ getNotes/__init__.py
@@ -10,9 +10,13 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request for getting all notes!')
     try:
         collection = DatabaseHelper.getCollection()
-        result = collection.find({})
-        result = dumps(result)
-        
-        return  func.HttpResponse(result, mimetype="application/json", charset='utf-8', status_code=200)
+        notes = collection.find({})
+        notesList = list(notes)
+        i = 1
+        for note in notesList:
+            note['title'] += '_test'+'_' + str(i)
+            i+=1
+        notes = dumps(notesList)      
+        return  func.HttpResponse(notes, mimetype="application/json", charset='utf-8', status_code=200)
     except ConnectionError:
         return func.HttpResponse("Unable to connect to the Mongo Db", status_code=400)
